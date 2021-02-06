6e4ffba621a067fe95b55826b7b21abe841c0c84
hafis915 <hafistafsani915uh@gmail.com>
2021-02-06 09:13:35 +0800

    add login page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { 
+  IonContent, 
+  IonPage,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonButton 
+} from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="login">
+        <div className="login-Page">
+          <div className="contain">
+            <div className="title">
+              <h1>Welcome</h1>
+              <h1
+              className= 'second-title'
+              >Back</h1>
+            </div>
+            <IonItem className="ionItem">
+              <IonLabel  
+              position="floating">Phone Number</IonLabel>
+              <IonInput 
+              ></IonInput>
+            </IonItem>
+            <IonItem className="ionItem">
+              <IonLabel  
+              position="floating">Pasword</IonLabel>
+              <IonInput 
+              ></IonInput>
+            </IonItem>
+            <div className="button">
+              <IonButton
+              className= "button-login"
+              expand="full"
+              >Login</IonButton>
+            </div>
+            <div className="button">
+              <IonButton
+              color="danger"
+              expand="full"
+              >Cancel</IonButton>
+            </div>
+          </div>
+
+        </div>
       </IonContent>
     </IonPage>
   );
